f3110fb66751244a0452011900eee1ce5f071b1d
sinnakeWEB <sinnakeWEB@sinnake.test.co.kr>
2018-12-31 17:53:21 +0900

    챕터 7 시작

--- ch07-jpa-start1/src/main/java/jpabook/start/mappedSuperClass/entity/Seller.java
+++ ch07-jpa-start1/src/main/java/jpabook/start/mappedSuperClass/entity/Seller.java
@@ -3,17 +3,10 @@ package jpabook.start.mappedSuperClass.entity;
 import javax.persistence.AttributeOverride;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.SequenceGenerator;
 
 import jpabook.start.mappedSuperClass.entity.abs.BaseEntity;
 
 @Entity(name = "CH07_MAPPED_SUPER_CLASS_SELLER")
-@SequenceGenerator(
-	name = "CH07_MAPPED_SUPER_CLASS_SEQUENCE"
-	, sequenceName = "CH07_MAPPED_SUPER_CLASS_SEQ"
-	, initialValue = 1
-	, allocationSize = 1
-)
 @AttributeOverride(name = "id", column = @Column(name = "SELLER_ID"))
 public class Seller extends BaseEntity {
 	
